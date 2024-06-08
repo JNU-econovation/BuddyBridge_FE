5c547b8bbf9297b63c0e4e78232406ca3cb0c991
minbo <smb5174@naver.com>
2024-06-09 00:30:23 +0900

    refactor: color 변경

--- src/components/common/Header/User/User.tsx
+++ src/components/common/Header/User/User.tsx
@@ -1,7 +1,6 @@
-import useUserInfoStore from "@/stores/kakaoInnfo";
-
-import NotLogin from "@/components/common/Header/User/NotLogin/NotLogin";
 import Login from "@/components/common/Header/User/Login/Login";
+import NotLogin from "@/components/common/Header/User/NotLogin/NotLogin";
+import useUserInfoStore from "@/stores/kakaoInnfo";
 
 export default function User() {
   const { userInfo } = useUserInfoStore();
