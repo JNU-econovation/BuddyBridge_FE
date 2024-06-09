fa3ba54c4a190a92a259e07056681a19592b60a8
minbo <smb5174@naver.com>
2024-06-09 13:26:06 +0900

    feat: 도와줄래요? 컴포넌트 구현

---
+++ src/components/page-layout/HomeLayout/components/PostList/GiverPostList/GiverPostList.tsx
@@ -0,0 +1,74 @@
+import classNames from "classnames/bind";
+
+import Link from "next/link";
+
+import Post from "@/components/common/Post/Post";
+import styles from "@/components/page-layout/HomeLayout/components/PostList/GiverPostList/GiverPostList.module.scss";
+import Plus from "@/icons/plus.svg";
+
+const cn = classNames.bind(styles);
+
+const mockData = [
+  {
+    title: "같이 이동해 ‘다원’에서 점심 식사를 할 사람~ 구합니다!",
+    disability: "지체 장애",
+    help: "생활",
+    place: "광주광역시 북구",
+    startDate: "2024.05.05",
+    endDate: "2024.05.07",
+    period: "정기",
+    order: 1,
+    type: "giver",
+  },
+  {
+    title: "같이 이동해 ‘다원’에서 점심 식사를 할 사람~ 구합니다!",
+    disability: "지체 장애",
+    help: "생활",
+    place: "광주광역시 북구",
+    startDate: "2024.05.05",
+    endDate: "2024.05.07",
+    period: "정기",
+    order: 1,
+    type: "giver",
+  },
+  {
+    title: "같이 이동해 ‘다원’에서 점심 식사를 할 사람~ 구합니다!",
+    disability: "지체 장애",
+    help: "생활",
+    place: "광주광역시 북구",
+    startDate: "2024.05.05",
+    endDate: "2024.05.07",
+    period: "정기",
+    order: 1,
+    type: "giver",
+  },
+  {
+    title: "같이 이동해 ‘다원’에서 점심 식사를 할 사람~ 구합니다!",
+    disability: "지체 장애",
+    help: "생활",
+    place: "광주광역시 북구",
+    startDate: "2024.05.05",
+    endDate: "2024.05.07",
+    period: "정기",
+    order: 1,
+    type: "giver",
+  },
+];
+
+export default function GiverPostList() {
+  return (
+    <div className={cn("container")}>
+      <header className={cn("header")}>
+        <p>도와줄래요?</p>
+        <Link href="/">
+          <Plus />
+        </Link>
+      </header>
+      <div className={cn("postListBox")}>
+        {mockData.map((post, idx) => (
+          <Post data={post} key={idx} />
+        ))}
+      </div>
+    </div>
+  );
+}
