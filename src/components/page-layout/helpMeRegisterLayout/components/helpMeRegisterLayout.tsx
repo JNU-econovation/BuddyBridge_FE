5f0149ffa85b3a28b0b20fba5abd11dc5632c3da
minbo <smb5174@naver.com>
2024-06-09 23:29:53 +0900

    refactor: UI 변경

--- src/components/page-layout/helpMeRegisterLayout/components/helpMeRegisterLayout.tsx
+++ src/components/page-layout/helpMeRegisterLayout/components/helpMeRegisterLayout.tsx
@@ -10,8 +10,8 @@ import Input from "@/components/common/Input/Input";
 import Label from "@/components/common/Label/Label";
 import RadioInput from "@/components/common/RadioInput/RadioInput";
 import Textarea from "@/components/common/Textarea/Textarea";
+import styles from "@/components/page-layout/helpMeRegisterLayout/components/helpMeRegisterLayout.module.scss";
 import DropDownImg from "@/icons/dropdown.svg";
-import styles from "@/pages/help-me/register/HelpMeRegister.module.scss";
 
 const cn = classNames.bind(styles);
 
@@ -20,145 +20,141 @@ export default function HelpMeRegisterLayout() {
 
   return (
     <div className={cn("container")}>
-      <p className={cn("title")}>도와줄래요?폼 작성</p>
-      <div className={cn("contentContainer")}>
-        <div className={cn("formContainer")}>
-          <Button className={cn("infoButton")}>내 정보 불러오기</Button>
-          <form className={cn("form")} onSubmit={handleSubmit((data) => console.log(data))}>
-            <div className={cn("titleContainer")}>
-              <Label className={cn("label")} htmlFor="title">
-                제목
+      <div className={cn("box")}>
+        <p className={cn("title")}>도와줄래요? 리스트 작성</p>
+        <form className={cn("form")} onSubmit={handleSubmit((data) => console.log(data))}>
+          <Button type="button" className={cn("getMyInfo")}>
+            내 정보 불러오기
+          </Button>
+          <div className={cn("titleContainer")}>
+            <Label className={cn("label")} htmlFor="title">
+              제목
+            </Label>
+            <Input
+              className={cn("titleInput")}
+              id="title"
+              placeholder="구체적으로 필요한 도움을 적어주세요. 예) 이동 도움 필요"
+              {...register("title")}
+            />
+          </div>
+          <div className={cn("genderAgeContainer")}>
+            <div className={cn("genderContainer")}>
+              <Label className={cn("label")} htmlFor="gender">
+                성별
               </Label>
-              <Input
-                className={cn("titleInput")}
-                id="title"
-                placeholder="구체적으로 필요한 도움을 적어주세요. 예) 이동 도움 필요"
-                {...register("title")}
+              <Controller
+                name="gender"
+                control={control}
+                render={({ field }) => <RadioInput {...field} firstValue="남성" secondValue="여성" />}
               />
             </div>
-            <div className={cn("genderAgeContainer")}>
-              <div className={cn("genderContainer")}>
-                <Label className={cn("label")} htmlFor="gender">
-                  성별
-                </Label>
-                <Controller
-                  name="gender"
-                  control={control}
-                  render={({ field }) => <RadioInput {...field} firstValue="남성" secondValue="여성" />}
-                />
-              </div>
-              <div className={cn("ageContainer")}>
-                <Label className={cn("label")} htmlFor="age">
-                  나이
-                </Label>
-                <Input className={cn("ageInput")} id="age" placeholder="숫자만 입력" {...register("age")} />
-              </div>
-              <div className={cn("disabilityContainer")}>
-                <Label className={cn("label")} htmlFor="disability">
-                  장애 유형
-                </Label>
-                <Dropdown
-                  options={DISABILITY}
-                  onSelection={(option) => setValue("disability", option)}
-                  {...register("disability")}
-                />
-              </div>
-              <div className={cn("helpTypeContainer")}>
-                <Label className={cn("label")} htmlFor="assistanceType">
-                  도움 유형
-                </Label>
-                <Controller
-                  name="assistanceType"
-                  control={control}
-                  render={({ field }) => <RadioInput {...field} firstValue="교육" secondValue="생활" />}
-                />
-              </div>
-              <div className={cn("periodContainer")}>
-                <Label className={cn("label")} htmlFor="scheduleType ">
-                  주기 구분
-                </Label>
-                <Controller
-                  name="scheduleType"
-                  control={control}
-                  render={({ field }) => <RadioInput {...field} firstValue="정기" secondValue="비정기" />}
-                />
-              </div>
-              <Input
-                className={cn("periodDetailInput")}
-                id="scheduleDetail"
-                placeholder="예) 1째주, 화목"
-                {...register("scheduleDetail")}
-              />
+            <div className={cn("ageContainer")}>
+              <Label className={cn("label")} htmlFor="age">
+                나이
+              </Label>
+              <Input className={cn("ageInput")} id="age" placeholder="숫자만 입력" {...register("age")} />
             </div>
-            <div className={cn("placeContainer")}>
-              <Label className={cn("label")} htmlFor="district">
-                장소
+            <div className={cn("disabilityContainer")}>
+              <Label className={cn("label")} htmlFor="disability">
+                장애 유형
               </Label>
               <Dropdown
-                options={PLACE}
-                onSelection={(option) => setValue("district", option)}
-                {...register("district")}
+                options={DISABILITY}
+                onSelection={(option) => setValue("disability", option)}
+                {...register("disability")}
               />
             </div>
-            <div className={cn("dateContainer")}>
-              <Label className={cn("label")} htmlFor="date">
-                기간
+            <div className={cn("helpTypeContainer")}>
+              <Label className={cn("label")} htmlFor="assistanceType">
+                도움 유형
               </Label>
-              <div className={cn("dateBox")}>
-                <div className={cn("date")}>
-                  <Controller
-                    name="startDate"
-                    control={control}
-                    render={({ field }) => (
-                      <CustomDatePicker
-                        locale={ko}
-                        selected={field.value}
-                        onChange={field.onChange}
-                        dateFormat="yyyy년 MM월 dd일"
-                        customInputRef={field.ref}
-                      />
-                    )}
-                  />
-                  <DropDownImg className={cn("dropDownImg")} />
-                </div>
-                ~
-                <div className={cn("date")}>
-                  <Controller
-                    name="endDate"
-                    control={control}
-                    render={({ field }) => (
-                      <CustomDatePicker
-                        locale={ko}
-                        selected={field.value}
-                        onChange={field.onChange}
-                        dateFormat="yyyy년 MM월 dd일"
-                        customInputRef={field.ref}
-                      />
-                    )}
-                  />
-                  <DropDownImg className={cn("dropDownImg")} />
-                </div>
+              <Controller
+                name="assistanceType"
+                control={control}
+                render={({ field }) => <RadioInput {...field} firstValue="교육" secondValue="생활" />}
+              />
+            </div>
+            <div className={cn("periodContainer")}>
+              <Label className={cn("label")} htmlFor="scheduleType ">
+                주기 구분
+              </Label>
+              <Controller
+                name="scheduleType"
+                control={control}
+                render={({ field }) => <RadioInput {...field} firstValue="정기" secondValue="비정기" />}
+              />
+            </div>
+            <Input
+              className={cn("periodDetailInput")}
+              id="scheduleDetail"
+              placeholder="예) 1째주, 화목"
+              {...register("scheduleDetail")}
+            />
+          </div>
+          <div className={cn("placeContainer")}>
+            <Label className={cn("label")} htmlFor="district">
+              장소
+            </Label>
+            <Dropdown
+              options={PLACE}
+              onSelection={(option) => setValue("district", option)}
+              {...register("district")}
+            />
+          </div>
+          <div className={cn("dateContainer")}>
+            <Label className={cn("label")} htmlFor="date">
+              기간
+            </Label>
+            <div className={cn("dateBox")}>
+              <div className={cn("date")}>
+                <Controller
+                  name="startDate"
+                  control={control}
+                  render={({ field }) => (
+                    <CustomDatePicker
+                      locale={ko}
+                      selected={field.value}
+                      onChange={field.onChange}
+                      dateFormat="yyyy년 MM월 dd일"
+                      customInputRef={field.ref}
+                    />
+                  )}
+                />
+                <DropDownImg className={cn("dropDownImg")} />
+              </div>
+              ~
+              <div className={cn("date")}>
+                <Controller
+                  name="endDate"
+                  control={control}
+                  render={({ field }) => (
+                    <CustomDatePicker
+                      locale={ko}
+                      selected={field.value}
+                      onChange={field.onChange}
+                      dateFormat="yyyy년 MM월 dd일"
+                      customInputRef={field.ref}
+                    />
+                  )}
+                />
+                <DropDownImg className={cn("dropDownImg")} />
               </div>
             </div>
-            <div className={cn("detailContainer")}>
-              <Label className={cn("label")} htmlFor="content">
-                내용
-              </Label>
-              <Textarea
-                placeholder="도움이 필요한 정보를 상세하게 적어주세요. (인원/ 시간/ 세부 장소/ 도움 필요 내용)
+          </div>
+          <div className={cn("detailContainer")}>
+            <Label className={cn("label")} htmlFor="content">
+              내용
+            </Label>
+            <Textarea
+              placeholder="도움이 필요한 정보를 상세하게 적어주세요. (인원/ 시간/ 세부 장소/ 도움 필요 내용)
 ex, 2시에 전대치과병원에서 진료 이동 도움이 필요합니다."
-                id="content"
-                className={cn("detailTextarea")}
-                {...register("content")}
-              />
-            </div>
-            <div className={cn("buttonContainer")}>
-              <Button type="submit" className={cn("button")}>
-                제출하기
-              </Button>
-            </div>
-          </form>
-        </div>
+              id="content"
+              className={cn("detailTextarea")}
+              {...register("content")}
+            />
+          </div>
+          <Button className={cn("registerBox")}>등록하기</Button>
+        </form>
       </div>
     </div>
   );
