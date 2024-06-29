15e33aa35c22d99c1d941084fb63e8373d4896c1
minbo <smb5174@naver.com>
2024-06-29 20:25:13 +0900

    refactor: 프로퍼티 네임 수정

--- src/components/common/Post/Post.tsx
+++ src/components/common/Post/Post.tsx
@@ -14,21 +14,20 @@ const cn = classNames.bind(styles);
 interface PostProps {
   data: {
     title: string;
-    disability: string;
-    help: string;
-    place: string;
-    startDate: string;
-    endDate: string;
-    period: string;
-    order: number;
-    type: string;
-    id: number;
+    disabilityType: string;
+    assistanceType: string;
+    district: string;
+    startTime: string;
+    endTime: string;
+    scheduleType: string;
+    postId: number;
+    postType: string;
   };
 }
 
 export default function Post({ data }: PostProps) {
   const [isHeartClick, setIsHeartClick] = useState(false);
-  const { type, title, disability, help, place, startDate, endDate, period, order, id } = data;
+  const { postType, title, disabilityType, assistanceType, district, startTime, endTime, scheduleType, postId } = data;
 
   const handleHeartClick = () => {
     setIsHeartClick((prev) => !prev);
@@ -36,36 +35,36 @@ export default function Post({ data }: PostProps) {
 
   return (
     <Link
-      href={type === "taker" ? `${ROUTE.HELP_ME}/${id}` : `${ROUTE.GIVER}/${id}`}
+      href={postType === "taker" ? `${ROUTE.HELP_ME}/${postId}` : `${ROUTE.GIVER}/${postId}`}
       className={cn("cardContainer", {
-        taker: type === "taker",
-        giver: type === "giver",
+        taker: postType === "taker",
+        giver: postType === "giver",
       })}
     >
       <div className={cn("contentContainer")}>
         <p
           className={cn("progress", {
-            taker: type === "taker",
-            giver: type === "giver",
+            taker: postType === "taker",
+            giver: postType === "giver",
           })}
         >
           매칭중
         </p>
         <Heart width={32} height={32} className={cn("heart")} />
-        <p className={cn("order")}>{order}</p>
+        <p className={cn("order")}>{postId}</p>
         <div className={cn("contentBox")}>
           <div className={cn("Box")}>
             <p className={cn("title")}>{title}</p>
             <p className={cn("hr")} />
             <div className={cn("detailBox")}>
-              <p>{`일시 | ${startDate} ~ ${endDate}`}</p>
-              <p>{`활동 | ${period}`}</p>
-              <p>{`장소 | ${place}`}</p>
+              <p>{`일시 | ${startTime} ~ ${endTime}`}</p>
+              <p>{`활동 | ${scheduleType}`}</p>
+              <p>{`장소 | ${district}`}</p>
             </div>
           </div>
           <div className={cn("hashtagBox")}>
-            <p>{`# ${help}`}</p>
-            <p>{`# ${disability}`}</p>
+            <p>{`# ${assistanceType}`}</p>
+            <p>{`# ${disabilityType}`}</p>
           </div>
         </div>
       </div>
