17b1e1825f94d3713b8e36dc8ec876cfa9f5f04c
minbo <smb5174@naver.com>
2024-06-30 17:48:35 +0900

    refactor: 경로 수정 및 추가

--- src/constants/route.ts
+++ src/constants/route.ts
@@ -1,7 +1,8 @@
 export const ROUTE = {
   HOME: "/",
   LOGIN: "/login",
-  HELP_ME: "help-me",
-  HELP_ME_REGISTER: "help-me/register",
-  GIVER: "giver",
+  HELP_ME: "/help-me",
+  HELP_ME_REGISTER: "/help-me/register",
+  HELP_ME_EDIT: "/help-me/edit",
+  GIVER: "/giver",
 } as const;
