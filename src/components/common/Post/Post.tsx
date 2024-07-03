e8455027480369eb0b14a9eaec72253f43d5bcdb
minbo <smb5174@naver.com>
2024-07-03 18:39:01 +0900

    refactor: 함수 모듈화

--- src/components/common/Post/Post.tsx
+++ src/components/common/Post/Post.tsx
@@ -9,21 +9,12 @@ import PostData from "@/components/page-layout/HomeLayout/types";
 import { ROUTE } from "@/constants/route";
 import Heart from "@/icons/heart.svg";
 import RedHeart from "@/icons/red_haert.svg";
+import { formatDateString } from "@/utils";
 
 const cn = classNames.bind(styles);
 
 interface PostProps {
   data: PostData;
-}
-
-function formatDateString(dateString: string) {
-  const date = new Date(dateString);
-
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
-
-  return `${year}.${month}.${day}`;
 }
 
 export default function Post({ data }: PostProps) {
