2f199588e5909648a857b9de0116ea4853d4f287
minbo <smb5174@naver.com>
2024-07-03 19:15:25 +0900

    feat: 도와줄게요 페이지 api 연결

--- src/components/page-layout/helpYouLayout/components/helpYouLayout.tsx
+++ src/components/page-layout/helpYouLayout/components/helpYouLayout.tsx
@@ -4,6 +4,7 @@ import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import classNames from "classnames/bind";
 
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 import getPagenationItems from "@/components/common/Pagenation/apis/getHelpMeList";
 import Pagination from "@/components/common/Pagenation/Pagenation";
@@ -11,105 +12,29 @@ import Post from "@/components/common/Post/Post";
 import styles from "@/components/page-layout/helpYouLayout/components/helpYouLayout.module.scss";
 import { ROUTE } from "@/constants/route";
 
+import PostData from "../../HomeLayout/types";
+
 const cn = classNames.bind(styles);
 
-const mockData = [
-  {
-    title: "같이 이동해 ‘다원’에서 점심 식사를 할 사람~ 구합니다!",
-    disabilityType: "지체 장애",
-    assistanceType: "생활",
-    district: "광주광역시 북구",
-    startTime: "2024.05.05",
-    endTime: "2024.05.07",
-    scheduleType: "정기",
-    postType: "giver",
-    postId: 1,
-  },
-  {
-    title: "같이 이동해 ‘다원’에서 점심 식사를 할 사람~ 구합니다!",
-    disabilityType: "지체 장애",
-    assistanceType: "생활",
-    district: "광주광역시 북구",
-    startTime: "2024.05.05",
-    endTime: "2024.05.07",
-    scheduleType: "정기",
-    postType: "giver",
-    postId: 2,
-  },
-  {
-    title: "같이 이동해 ‘다원’에서 점심 식사를 할 사람~ 구합니다!",
-    disabilityType: "지체 장애",
-    assistanceType: "생활",
-    district: "광주광역시 북구",
-    startTime: "2024.05.05",
-    endTime: "2024.05.07",
-    scheduleType: "정기",
-    postType: "giver",
-    postId: 3,
-  },
-  {
-    title: "같이 이동해 ‘다원’에서 점심 식사를 할 사람~ 구합니다!",
-    disabilityType: "지체 장애",
-    assistanceType: "생활",
-    district: "광주광역시 북구",
-    startTime: "2024.05.05",
-    endTime: "2024.05.07",
-    scheduleType: "정기",
-    postType: "giver",
-    postId: 4,
-  },
-  {
-    title: "같이 이동해 ‘다원’에서 점심 식사를 할 사람~ 구합니다!",
-    disabilityType: "지체 장애",
-    assistanceType: "생활",
-    district: "광주광역시 북구",
-    startTime: "2024.05.05",
-    endTime: "2024.05.07",
-    scheduleType: "정기",
-    postType: "giver",
-    postId: 5,
-  },
-  {
-    title: "같이 이동해 ‘다원’에서 점심 식사를 할 사람~ 구합니다!",
-    disabilityType: "지체 장애",
-    assistanceType: "생활",
-    district: "광주광역시 북구",
-    startTime: "2024.05.05",
-    endTime: "2024.05.07",
-    scheduleType: "정기",
-    postType: "giver",
-    postId: 6,
-  },
-  {
-    title: "같이 이동해 ‘다원’에서 점심 식사를 할 사람~ 구합니다!",
-    disabilityType: "지체 장애",
-    assistanceType: "생활",
-    district: "광주광역시 북구",
-    startTime: "2024.05.05",
-    endTime: "2024.05.07",
-    scheduleType: "정기",
-    postType: "giver",
-    postId: 7,
-  },
-  {
-    title: "같이 이동해 ‘다원’에서 점심 식사를 할 사람~ 구합니다!",
-    disabilityType: "지체 장애",
-    assistanceType: "생활",
-    district: "광주광역시 북구",
-    startTime: "2024.05.05",
-    endTime: "2024.05.07",
-    scheduleType: "정기",
-    postType: "giver",
-    postId: 8,
-  },
-];
-
 export default function HelpYouLayout() {
-  const [page, setPage] = useState(1);
+  const router = useRouter();
+  const params = new URLSearchParams(router.query as any);
+  const currentPage = params.get("page");
+  const page = Number(currentPage) || 0;
+  console.log(params);
+
+  const setPage = (newPage: number) => {
+    const pathName = router.pathname;
+    params.set("page", newPage.toString());
+    router.replace({
+      pathname: pathName,
+      query: { ...Object.fromEntries(params.entries()) },
+    });
+  };
 
   const { data } = useQuery({
-    queryKey: ["items", page],
-    queryFn: () => getPagenationItems(page, 8),
+    queryKey: ["post", page],
+    queryFn: () => getPagenationItems("GIVER", page, 8),
     placeholderData: keepPreviousData,
   });
 
@@ -134,14 +59,20 @@ export default function HelpYouLayout() {
       </div>
       <div className={cn("cardListContainer")}>
         <div className={cn("cardListBox")}>
-          {mockData.map((post, idx) => (
-            <Post data={post} key={idx} />
+          {data?.data.content.map((post: PostData) => (
+            <Post data={post} key={post.id} />
           ))}
           <Link href={ROUTE.HELP_YOU_REGISTER} className={cn("button")}>
             작성하기
           </Link>
         </div>
-        <Pagination currentPage={page} itemsPerPage={8} totalItems={20} setPage={setPage} type="giver" />
+        <Pagination
+          type="giver"
+          currentPage={page + 1}
+          itemsPerPage={8}
+          totalItems={data?.data.totalElements}
+          setPage={setPage}
+        />
       </div>
     </main>
   );
