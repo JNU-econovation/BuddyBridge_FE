1488d5e5b84138d70fee7d32e33a1c5bbe92e702
minbo <smb5174@naver.com>
2024-07-11 17:33:34 +0900

    feat: 댓글 컴포넌트 구현

---
+++ src/components/common/Comment/Comment.tsx
@@ -0,0 +1,72 @@
+import { useEffect, useRef, useState } from "react";
+
+import classNames from "classnames/bind";
+
+import Image from "next/image";
+
+import styles from "@/components/common/Comment/Comment.module.scss";
+import useOutsideClick from "@/hooks/useOutsideClick";
+import Kebab from "@/icons/kebab.svg";
+import useUserInfoStore from "@/stores/kakaoInnfo";
+import { formatDateString } from "@/utils";
+
+import Chat from "./Chat/Chat";
+
+const cn = classNames.bind(styles);
+
+export interface CommentProps {
+  comment: {
+    author: {
+      memberId: number;
+      nickname: string;
+      profileImg: string;
+    };
+    content: string;
+    modifiedAt: string;
+    commentId: number;
+  };
+  postId: number;
+}
+
+export default function Comment({ comment, postId }: CommentProps) {
+  const [isKebabClick, setIsKebabClick] = useState(false);
+  const kebabRef = useRef<HTMLDivElement>(null);
+  const editBoxRef = useRef<HTMLDivElement>(null);
+
+  const { userInfo } = useUserInfoStore();
+
+  const handleKebabClick = () => {
+    setIsKebabClick((prev) => !prev);
+  };
+
+  useOutsideClick([kebabRef, editBoxRef], () => setIsKebabClick(false));
+
+  return (
+    <div className={cn("container")}>
+      <div className={cn("box")}>
+        <div className={cn("img")}>
+          <Image fill src={comment.author.profileImg} alt="댓글 작성자 프로필" />
+        </div>
+        <div className={cn("contentBox")}>
+          <div className={cn("titleBox")}>
+            <p className={cn("nickname")}>{comment.author.nickname}</p>
+            <p className={cn("date")}>{formatDateString(comment.modifiedAt)}</p>
+          </div>
+          <p className={cn("content")}>{comment.content}</p>
+        </div>
+        {comment.author.memberId === userInfo?.memberId && (
+          <div className={cn("kebabBox")} ref={kebabRef}>
+            <Kebab onClick={handleKebabClick} className={cn("kebab")} width={20} height={20} />
+            {isKebabClick && (
+              <div className={cn("editBox")} ref={editBoxRef}>
+                <button>수정</button>
+                <button>삭제</button>
+              </div>
+            )}
+          </div>
+        )}
+        {userInfo?.memberId === postId && <Chat />}
+      </div>
+    </div>
+  );
+}
