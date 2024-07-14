e2eead5259cec079d80cd62d6513e7fa61ad2e5a
minbo <smb5174@naver.com>
2024-07-14 14:51:04 +0900

    refactor: 댓글 get 요청 수정

--- src/components/page-layout/helpYouDetailLayout/apis/getAllComment.ts
+++ src/components/page-layout/helpYouDetailLayout/apis/getAllComment.ts
@@ -1,7 +1,7 @@
 import axiosInstance from "@/apis/axiosInstance";
 
 export default async function getAllComment(id: string, limit: number, cursor: number) {
-  if (cursor) {
+  if (cursor !== 0) {
     const { data } = await axiosInstance.get(`comments/${id}?limit=${limit}&cursor=${cursor}`);
     return data.data;
   } else {
