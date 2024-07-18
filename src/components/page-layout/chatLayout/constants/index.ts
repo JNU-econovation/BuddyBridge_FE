6cacf9094f9432ddcdf6dc9cb2109ce1c9bf43be
minbo <smb5174@naver.com>
2024-07-18 11:13:18 +0900

    feat: 채팅 헤더 상수 선언

---
+++ src/components/page-layout/chatLayout/constants/index.ts
@@ -0,0 +1,14 @@
+export const MATCHING_STATE = [
+  {
+    state: "전체",
+    engState: "all",
+  },
+  {
+    state: "매칭중",
+    engState: "ing",
+  },
+  {
+    state: "매칭완료",
+    engState: "finish",
+  },
+];
