b899fada63e05613a3eb0e8c6a181bb90c5f24de
minbo <smb5174@naver.com>
2024-07-18 22:37:14 +0900

    refactor: 헤더에 리액트 쿼리 적용

--- src/components/common/Header/User/User.tsx
+++ src/components/common/Header/User/User.tsx
@@ -1,9 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { useRouter } from "next/router";
+
 import Login from "@/components/common/Header/User/Login/Login";
 import NotLogin from "@/components/common/Header/User/NotLogin/NotLogin";
+import getKakaoInfo from "@/components/page-layout/HomeLayout/apis/getKakaoInfo";
 import useUserInfoStore from "@/stores/kakaoInnfo";
 
 export default function User() {
-  const { userInfo } = useUserInfoStore();
+  const { code } = useUserInfoStore();
 
-  return userInfo ? <Login name={userInfo.nickname} /> : <NotLogin />;
+  const { data } = useQuery({
+    queryKey: ["user"],
+    queryFn: () => getKakaoInfo(code as string),
+  });
+
+  return data?.nickname ? <Login name={data.nickname} /> : <NotLogin />;
 }
