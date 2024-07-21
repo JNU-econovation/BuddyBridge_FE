89eeffaf566ae582fcceec4dc0cae4b964a2a959
minbo <smb5174@naver.com>
2024-07-21 16:44:59 +0900

    feat: 알람 SSE 구현

--- src/components/common/Header/User/Login/Login.tsx
+++ src/components/common/Header/User/Login/Login.tsx
@@ -1,5 +1,6 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
+import axios from "axios";
 import classNames from "classnames/bind";
 
 import { useRouter } from "next/router";
@@ -10,6 +11,7 @@ import { ROUTE } from "@/constants/route";
 import Alarm from "@/icons/alarm.svg";
 import ArrowDown from "@/icons/arrow_down.svg";
 import Chat from "@/icons/chattig.svg";
+import useUserInfoStore from "@/stores/kakaoInnfo";
 
 import DropDown from "../DropDown/DropDown";
 
@@ -20,8 +22,12 @@ interface LoginProps {
 }
 
 export default function Login({ name }: LoginProps) {
+  const { userInfo } = useUserInfoStore();
   const dropdownRef = useRef(null);
   const [isOpen, setIsOpen] = useDetectClose(dropdownRef, false);
+  const [notifications, setNotifications] = useState<string[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [isConnected, setIsConnected] = useState(false);
   const router = useRouter();
 
   const handleNameClick = () => {
@@ -31,6 +37,56 @@ export default function Login({ name }: LoginProps) {
   const handleChatClick = () => {
     router.push(ROUTE.CHAT);
   };
+
+  // SSE 연결 및 알림 수신
+  useEffect(() => {
+    if (!userInfo) return; // userInfo가 없으면 연결하지 않음
+
+    let eventSource: EventSource;
+    let lastEventId = "";
+
+    const connectSSE = () => {
+      eventSource = new EventSource(`${process.env.NEXT_PUBLIC_BASE_URL}api/sse/connect`, {
+        withCredentials: true,
+      });
+
+      eventSource.addEventListener("notification", (event) => {
+        const newNotification = event.data;
+        console.log("새로운 알림:", newNotification);
+        try {
+          const parsedData = JSON.parse(newNotification);
+          console.log(parsedData);
+        } catch (error) {
+          return;
+        }
+
+        setNotifications((prevNotifications) => [...prevNotifications, newNotification]);
+        lastEventId = event.lastEventId;
+      });
+
+      eventSource.onerror = (error) => {
+        console.error("SSE error:", error);
+        setError("연결에 실패했습니다. 재연결 중...");
+        setIsConnected(false);
+        eventSource.close();
+        setTimeout(connectSSE, 5000); // 5초 후 재연결 시도
+      };
+
+      eventSource.onopen = () => {
+        setError(null);
+        setIsConnected(true);
+        console.log("SSE 연결 성공");
+      };
+    };
+
+    connectSSE(); // SSE 연결 시도
+
+    return () => {
+      if (eventSource) {
+        eventSource.close();
+      }
+    };
+  }, [userInfo]); // userInfo가 변경될 때마다 effect 실행
 
   return (
     <div className={cn("container")}>
