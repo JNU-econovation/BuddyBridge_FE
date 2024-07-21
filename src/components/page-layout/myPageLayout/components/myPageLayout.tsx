1e934dd3ed438e74c2275b261cfa60e0e7d2d66d
minbo <smb5174@naver.com>
2024-07-21 23:49:55 +0900

    feat: 로그인이 안돼있으면 로그인페이지로 이동

--- src/components/page-layout/myPageLayout/components/myPageLayout.tsx
+++ src/components/page-layout/myPageLayout/components/myPageLayout.tsx
@@ -1,6 +1,11 @@
 import classNames from "classnames/bind";
 
+import { useRouter } from "next/router";
+
+import openToast from "@/components/common/Toast/features/openToast";
 import styles from "@/components/page-layout/myPageLayout/components/myPageLayout.module.scss";
+import { ROUTE } from "@/constants/route";
+import useUserInfoStore from "@/stores/kakaoInnfo";
 
 import MyInfo from "./MyInfo/MyInfo";
 import MyPageNav from "./MyPageNav/MyPageNav";
@@ -8,6 +13,13 @@ import MyPageNav from "./MyPageNav/MyPageNav";
 const cn = classNames.bind(styles);
 
 export default function MyPageLayout() {
+  const { userInfo } = useUserInfoStore();
+  const router = useRouter();
+
+  if (!userInfo) {
+    router.push(ROUTE.LOGIN);
+  }
+
   return (
     <div className={cn("container")}>
       <MyPageNav />
