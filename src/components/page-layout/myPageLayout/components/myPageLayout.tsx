199dd136f216e56906fa0adbd8290f29fc516984
minbo <smb5174@naver.com>
2024-07-22 11:33:28 +0900

    refactor: router 에러 수정

--- src/components/page-layout/myPageLayout/components/myPageLayout.tsx
+++ src/components/page-layout/myPageLayout/components/myPageLayout.tsx
@@ -1,8 +1,9 @@
+import { useEffect } from "react";
+
 import classNames from "classnames/bind";
 
 import { useRouter } from "next/router";
 
-import openToast from "@/components/common/Toast/features/openToast";
 import styles from "@/components/page-layout/myPageLayout/components/myPageLayout.module.scss";
 import { ROUTE } from "@/constants/route";
 import useUserInfoStore from "@/stores/kakaoInnfo";
@@ -16,9 +17,11 @@ export default function MyPageLayout() {
   const { userInfo } = useUserInfoStore();
   const router = useRouter();
 
-  if (!userInfo) {
-    router.push(ROUTE.LOGIN);
-  }
+  useEffect(() => {
+    if (!userInfo) {
+      router.push(ROUTE.LOGIN);
+    }
+  }, [userInfo, router]);
 
   return (
     <div className={cn("container")}>
