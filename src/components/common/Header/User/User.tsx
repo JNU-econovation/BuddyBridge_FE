66cfabb9a5374c52161065b0fd6f15bc262481f9
minbo <smb5174@naver.com>
2024-07-22 11:34:45 +0900

    feat: 헤더에 로그인 정보를 받아올 때 로딩 처리

--- src/components/common/Header/User/User.tsx
+++ src/components/common/Header/User/User.tsx
@@ -5,14 +5,20 @@ import NotLogin from "@/components/common/Header/User/NotLogin/NotLogin";
 import getKakaoInfo from "@/components/page-layout/HomeLayout/apis/getKakaoInfo";
 import useUserInfoStore from "@/stores/kakaoInnfo";
 
+import Loader from "../../Loader/Loader";
+
 export default function User() {
   const { code } = useUserInfoStore();
 
-  const { data } = useQuery({
+  const { data, isPending } = useQuery({
     queryKey: ["user"],
     queryFn: () => getKakaoInfo(code as string),
     enabled: !!code,
   });
+
+  if (isPending) {
+    return <Loader />;
+  }
 
   return data?.nickname ? <Login name={data.nickname} /> : <NotLogin />;
 }
