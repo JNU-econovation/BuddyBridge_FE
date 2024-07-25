bf82e5b541279bc49d3166562f6b521442690079
minbo <smb5174@naver.com>
2024-07-25 21:22:09 +0900

    refactor: 메인페이지에서 불러오는 게시물 api 쿼리 파라미터 추가

--- src/components/page-layout/HomeLayout/apis/getGiverPost.ts
+++ src/components/page-layout/HomeLayout/apis/getGiverPost.ts
@@ -1,6 +1,8 @@
 import axiosInstance from "@/apis/axiosInstance";
 
 export default async function getGiverPost() {
-  const { data } = await axiosInstance.get("posts?post-type=GIVER&page=0&size=4&sorted=modifiedAt,DESC");
+  const { data } = await axiosInstance.get(
+    "posts?post-type=GIVER&page=1&size=4&sorted=modifiedAt,DESC&post-status=RECRUITING",
+  );
   return data.data.content;
 }
