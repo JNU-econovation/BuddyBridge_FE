f5d152080795c39a207149d3a678ce297f123c00
minbo <smb5174@naver.com>
2024-07-26 11:37:15 +0900

    feat: 파비콘 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,14 @@ import { Metadata } from "next";
 
 import RootLayout from "@/components/common/RootLayout/RootLayout";
 import HomeLayout from "@/components/page-layout/HomeLayout/components/HomeLayout";
+import Favicon from "@/images/loginImg.svg";
 
 export const metadata: Metadata = {
   title: "Buddy Bridge",
   description: "장애인을 도와주는 봉사 플랫폼입니다.",
+  icons: {
+    icon: Favicon,
+  },
 };
 
 export default function Home() {
