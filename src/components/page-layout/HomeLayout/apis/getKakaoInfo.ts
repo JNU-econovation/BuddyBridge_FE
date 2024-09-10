22edb0e55b4673e26886af830e4bfd79d7459416
minbo <smb5174@naver.com>
2024-09-10 17:49:18 +0900

    refactor: 카카오 로그인 api 수정

--- src/components/page-layout/HomeLayout/apis/getKakaoInfo.ts
+++ src/components/page-layout/HomeLayout/apis/getKakaoInfo.ts
@@ -1,17 +1,12 @@
 import axiosInstance from "@/apis/axiosInstance";
 
 export default async function getKakaoInfo(code: string) {
-  if (code != "1") {
-    const { data } = await axiosInstance.post(
-      "oauth/login",
-      { authorizationCode: code },
-      {
-        withCredentials: true,
-      },
-    );
-    return data.data;
-  } else {
-    const { data } = await axiosInstance.get("oauth/login/1");
-    return data.data;
-  }
+  const { data } = await axiosInstance.post(
+    "oauth/login",
+    { authorizationCode: code },
+    {
+      withCredentials: true,
+    },
+  );
+  return data.data;
 }
