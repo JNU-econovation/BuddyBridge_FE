5dd208f281302e9cb3c97006550cba045841bdbd
minbo <smb5174@naver.com>
2024-09-11 15:59:42 +0900

    feat: 테스트 로그인 구현

--- src/components/page-layout/loginLayout/components/loginLayout.tsx
+++ src/components/page-layout/loginLayout/components/loginLayout.tsx
@@ -1,3 +1,5 @@
+import { FormEvent, useState } from "react";
+
 import classNames from "classnames/bind";
 
 import { useRouter } from "next/router";
@@ -7,14 +9,23 @@ import { ROUTE } from "@/constants/route";
 import Kakao from "@/icons/kakao.svg";
 import LoginImg from "@/images/loginImg.svg";
 
+import getTestLogin from "../apis/getTestLogin";
+
 const cn = classNames.bind(styles);
 
 export default function LoginLayout() {
+  const [userId, setUserId] = useState<string>();
   const router = useRouter();
   const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_Rest_api_key}&redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URI}&response_type=code`;
 
   const handleLogin = () => {
     window.location.href = kakaoURL;
+  };
+
+  const handleTestLogin = async (e: FormEvent) => {
+    e.preventDefault();
+    await getTestLogin(userId as string);
+    router.push(`${ROUTE.HOME}`);
   };
 
   return (
@@ -40,6 +51,18 @@ export default function LoginLayout() {
                 <Kakao className={cn("kakao")} />
                 <p className={cn("kakaoLogin")}>카카오 로그인</p>
               </button>
+              {process.env.NEXT_PUBLIC_BASE_URL == "https://buddybridge.13.209.34.25.sslip.io/" && (
+                <form onSubmit={handleTestLogin} className={cn("testButton")}>
+                  <input
+                    placeholder="userId 입력"
+                    className={cn("testLoginInput")}
+                    onChange={(e) => setUserId(e.target.value)}
+                  />
+                  <button className={cn("testLoginButton")} type="submit">
+                    테스트 로그인
+                  </button>
+                </form>
+              )}
             </div>
           </div>
           <p className={cn("loginInfo")}>※ 사용자의 신원을 보장하기 위해 카카오 로그인만 제공합니다. </p>
