5b5009a271c6923e21a2849c01283ccd8235f607
suuuuui <102002747+suuuin@users.noreply.github.com>
2024-09-12 12:26:36 +0900

    Delete src/components/common/Filter/apis/filterTakerPost.ts

--- src/components/common/Filter/apis/filterTakerPost.ts
+++
@@ -1,8 +0,0 @@
-import axiosInstance from "@/apis/axiosInstance";
-import { FilterParams } from "./filterGiverPost";
-
-export default async function filterGiverPost({disabilityType, assistanceType}:FilterParams) {
-    const { data } = await axiosInstance.get(
-        `posts?post-type=TAKER&disabilityType=${disabilityType}&assistanceType=${assistanceType}`,);
-    return data.data.content;
-}
