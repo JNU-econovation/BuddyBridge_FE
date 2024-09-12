e1c56282b94ea6588aa0df0381242d59a5e30941
minbo <smb5174@naver.com>
2024-09-12 10:05:58 +0900

    fix: 로그인 정보가 필요한 페이지에서 새로 고침 시 로그인 페이지로 이동하는 오류 수정

--- src/components/page-layout/myPageHelpMeLayout/components/myPageHelpMeLayout.tsx
+++ src/components/page-layout/myPageHelpMeLayout/components/myPageHelpMeLayout.tsx
@@ -1,13 +1,35 @@
+import { useEffect } from "react";
+
+import { useQuery } from "@tanstack/react-query";
 import classNames from "classnames/bind";
 
+import { useRouter } from "next/router";
+
+import openToast from "@/components/common/Toast/features/openToast";
 import styles from "@/components/page-layout/myPageEditLayout/components/myPageEditLayout.module.scss";
+import { ROUTE } from "@/constants/route";
 
 import MyRegisterPost from "./MyRegisterPost/MyRegisterPost";
+import getMyInfo from "../../myPageEditLayout/apis/getMyInfo";
 import MyPageNav from "../../myPageLayout/components/MyPageNav/MyPageNav";
 
 const cn = classNames.bind(styles);
 
 export default function MyPageHelpMeLayout() {
+  const router = useRouter();
+
+  const { data: myInfoData, isFetching } = useQuery({
+    queryKey: ["userInfo"],
+    queryFn: () => getMyInfo(),
+  });
+
+  useEffect(() => {
+    if (!myInfoData && !isFetching) {
+      router.push(ROUTE.LOGIN);
+      openToast("error", "로그인을 해주세요.");
+    }
+  }, [myInfoData, router, isFetching]);
+
   return (
     <div className={cn("container")}>
       <MyPageNav />
