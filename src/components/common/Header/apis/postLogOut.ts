785c5eec0141e2809e59385aec37494b0ceb392b
minbo <smb5174@naver.com>
2024-09-12 23:03:00 +0900

    refactor: 로그아웃 api에 body 추가

--- src/components/common/Header/apis/postLogOut.ts
+++ src/components/common/Header/apis/postLogOut.ts
@@ -2,8 +2,12 @@ import axiosInstance from "@/apis/axiosInstance";
 
 // todo: 리턴 값에 대한 type 지정 필요
 export default async function postLogOut() {
-  const { data } = await axiosInstance.get("oauth/logout", {
-    withCredentials: true,
-  });
+  const { data } = await axiosInstance.post(
+    "oauth/logout",
+    {},
+    {
+      withCredentials: true,
+    },
+  );
   return data.data;
 }
