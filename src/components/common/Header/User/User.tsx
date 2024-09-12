fdb5b4e6cfea1616700c6d2cdafa3e07329450d7
minbo <smb5174@naver.com>
2024-09-12 23:33:55 +0900

    fix: 로그인 성공 시 헤더에 로그인한 유저의 정보가 바로 보이지 않는 문제 해결

--- src/components/common/Header/User/User.tsx
+++ src/components/common/Header/User/User.tsx
@@ -1,15 +1,24 @@
+import { useEffect } from "react";
+
 import { useQuery } from "@tanstack/react-query";
 
 import Login from "@/components/common/Header/User/Login/Login";
 import NotLogin from "@/components/common/Header/User/NotLogin/NotLogin";
+import useUserInfoStore from "@/stores/kakaoInnfo";
 
 import getLogIn from "../apis/getLogIn";
 
 export default function User() {
-  const { data, isError } = useQuery({
+  const { userInfo } = useUserInfoStore();
+
+  const { data, isError, refetch } = useQuery({
     queryKey: ["userLogIn"],
     queryFn: () => getLogIn(),
   });
+
+  useEffect(() => {
+    refetch();
+  }, [userInfo, refetch]);
 
   if (isError) {
     return <NotLogin />;
