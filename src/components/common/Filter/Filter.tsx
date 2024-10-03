30ae347cb972925f82e816b5a15e9a88249e36b2
suin <suin020226@naver.com>
2024-10-03 17:19:24 +0900

    refactor: forwardRef 방식 변경, package 파일 다운그레이드

--- src/components/common/Filter/Filter.tsx
+++ src/components/common/Filter/Filter.tsx
@@ -12,31 +12,34 @@ interface FilterProps {
     handleFilter: (category: string, optionId: string) => void;
 }
   
-export default forwardRef<HTMLDivElement, FilterProps>(
-    function Filter(props: FilterProps, ref: React.Ref<HTMLDivElement>) {
-        const {searchParams, handleFilter} = props;
+
+function Filtering(props: FilterProps, ref: React.Ref<HTMLDivElement>) {
+    const {searchParams, handleFilter} = props;
     
-        return (
-            <div className={cn("container")} ref={ref}>
-                {Object.entries(Categories).map(([categoryName, categoryOptions]) => {
-                    const selectedOption = searchParams.get(categoryName);
-                    return(
-                        <div className={cn("filterColumn", { "borderRight": categoryName === 'disabilityType'})} key={categoryName}>
-                            {categoryOptions.map((option) => (
-                                <div className={cn("optionLabel")} key={option} >
-                                    <input
-                                        className={cn("checkbox")}
-                                        type="checkbox"
-                                        checked={selectedOption ? selectedOption.includes(option) : false}
-                                        onChange={() => handleFilter(categoryName, option)}
-                                    />
-                                    {option}
-                                </div>
-                            ))}
-                        </div>
-                    );
-                })}
-            </div>
-        );
-    }
-);
+    return (
+        <div className={cn("container")} ref={ref}>
+            {Object.entries(Categories).map(([categoryName, categoryOptions]) => {
+                const selectedOption = searchParams.get(categoryName);
+                return(
+                    <div className={cn("filterColumn", { "borderRight": categoryName === 'disabilityType'})} key={categoryName}>
+                        {categoryOptions.map((option) => (
+                            <div className={cn("optionLabel")} key={option} >
+                                <input
+                                    className={cn("checkbox")}
+                                    type="checkbox"
+                                    checked={selectedOption ? selectedOption.includes(option) : false}
+                                    onChange={() => handleFilter(categoryName, option)}
+                                />
+                                {option}
+                            </div>
+                        ))}
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
+
+const Filter = forwardRef<HTMLDivElement, FilterProps>(Filtering);
+
+export default Filter;
