c8252da7b67666f91ec54a7fb27232a4790b5801
minbo <smb5174@naver.com>
2024-11-20 15:00:24 +0900

    refactor: 달력 헤더 스타일 변경

--- src/components/common/DatePicker/DatePicker.tsx
+++ src/components/common/DatePicker/DatePicker.tsx
@@ -1,13 +1,17 @@
 import { ComponentProps, Ref, forwardRef } from "react";
 
 import classNames from "classnames/bind";
-import { Locale } from "date-fns";
+import { getMonth, getYear, Locale } from "date-fns";
 import DatePicker from "react-datepicker";
 
-import "react-datepicker/dist/react-datepicker.css";
 import styles from "@/components/common/DatePicker/DatePicker.module.scss";
+import Arrow from "@/icons/arrow.svg";
+import "react-datepicker/dist/react-datepicker.css";
 
 const cn = classNames.bind(styles);
+
+const MONTHS = ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"];
+const YEARS = Array.from({ length: getYear(new Date()) + 1 - 1900 }, (_, i) => getYear(new Date()) - i);
 
 interface CustomDatePickerProps {
   locale: Locale;
@@ -49,6 +53,46 @@ export default forwardRef<HTMLInputElement, CustomDatePickerProps>(function Cust
       dateFormat={dateFormat}
       customInput={<CustomInput placeholders={placeholder} classNames={classNames} ref={customInputRef} />}
       showPopperArrow={false}
+      renderCustomHeader={({ date, changeYear, decreaseMonth, increaseMonth }) => (
+        <DatePickerHeader
+          increaseMonth={increaseMonth}
+          changeYear={changeYear}
+          date={date}
+          decreaseMonth={decreaseMonth}
+        />
+      )}
     />
   );
 });
+
+interface DatePickerHeaderProps {
+  date: Date;
+  changeYear: (year: number) => void;
+  decreaseMonth: () => void;
+  increaseMonth: () => void;
+}
+
+function DatePickerHeader({ date, changeYear, decreaseMonth, increaseMonth }: DatePickerHeaderProps) {
+  return (
+    <div className={cn("customHeaderContainer")}>
+      <div className={cn("monthYearBox")}>
+        <select value={getYear(date)} className={cn("year")} onChange={({ target: { value } }) => changeYear(+value)}>
+          {YEARS.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+        <span className={cn("month")}>{MONTHS[getMonth(date)]}</span>
+      </div>
+      <div className={cn("buttonBox")}>
+        <button type="button" onClick={decreaseMonth} className={cn("prevMonthButton")}>
+          <Arrow />
+        </button>
+        <button type="button" onClick={increaseMonth} className={cn("afterMonthButton")}>
+          <Arrow />
+        </button>
+      </div>
+    </div>
+  );
+}
