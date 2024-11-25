091c18ddd0f183ed8550da27f9dc1139e79a3179
minbo <smb5174@naver.com>
2024-11-25 22:11:52 +0900

    refactor: headers에 Authorization이 필요할 수도 있는 요청에 토큰 추가

--- src/components/page-layout/HomeLayout/apis/getGiverPost.ts
+++ src/components/page-layout/HomeLayout/apis/getGiverPost.ts
@@ -1,8 +1,15 @@
 import { axiosInstance } from "@/apis/axiosInstance";
 
 export default async function getGiverPost() {
+  const accessToken = localStorage.getItem("accessToken");
+
   const { data } = await axiosInstance.get(
     "posts?post-type=GIVER&page=1&size=4&sorted=modifiedAt,DESC&post-status=RECRUITING",
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    },
   );
   return data.data.content;
 }
--- src/components/page-layout/HomeLayout/apis/getTakerPost.ts
+++ src/components/page-layout/HomeLayout/apis/getTakerPost.ts
@@ -1,8 +1,15 @@
 import { axiosInstance } from "@/apis/axiosInstance";
 
 export default async function getTakerPost() {
+  const accessToken = localStorage.getItem("accessToken");
+
   const { data } = await axiosInstance.get(
     "posts?post-type=TAKER&page=1&size=4&sorted=modifiedAt,DESC&post-status=RECRUITING",
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    },
   );
   return data.data.content;
 }
