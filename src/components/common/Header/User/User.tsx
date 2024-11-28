5e263e6a271a4e7d7b87431d03f7c4a1f3d3db58
minbo <smb5174@naver.com>
2024-11-28 19:55:57 +0900

    refactor: 카카오 로그인 방식 변경

--- src/components/common/Header/User/User.tsx
+++ src/components/common/Header/User/User.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 import { useQuery } from "@tanstack/react-query";
 
 import Login from "@/components/common/Header/User/Login/Login";
@@ -6,16 +8,17 @@ import NotLogin from "@/components/common/Header/User/NotLogin/NotLogin";
 import getLogIn from "../apis/getLogIn";
 
 export default function User() {
-  let accessToken;
+  const [accessToken, setAccessToken] = useState<string | null>(null);
 
-  if (typeof window !== "undefined") {
-    accessToken = localStorage.getItem("accessToken");
-  }
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setAccessToken(localStorage.getItem("accessToken"));
+    }
+  }, []);
 
   const { data, isError } = useQuery({
     queryKey: ["userLogIn"],
     queryFn: () => getLogIn(),
-    enabled: !!accessToken,
   });
 
   if (!accessToken) {
--- src/components/page-layout/HomeLayout/components/HomeLayout.tsx
+++ src/components/page-layout/HomeLayout/components/HomeLayout.tsx
@@ -1,36 +1,19 @@
-import { useEffect } from "react";
-
-import { useQuery } from "@tanstack/react-query";
+import { useQueryClient } from "@tanstack/react-query";
 
 import { useRouter } from "next/router";
 
 import Banner from "@/components/page-layout/HomeLayout/components/Banner/Banner";
 import PostList from "@/components/page-layout/HomeLayout/components/PostList/PostList";
-import useUserInfoStore from "@/stores/kakaoInnfo";
-
-import getKakaoInfo from "../apis/getKakaoInfo";
 
 export default function HomeLayout() {
   const router = useRouter();
-  const { code } = router.query;
+  const queryClient = useQueryClient();
 
-  const { setUserInfo, setCode } = useUserInfoStore();
-
-  const { data, isLoading } = useQuery({
-    queryKey: ["user"],
-    queryFn: () => getKakaoInfo(code as string),
-    enabled: !!code,
-  });
-
-  useEffect(() => {
-    if (data) {
-      setUserInfo(data);
-      setCode(code as string);
-    }
-  }, [data, code, setUserInfo, setCode]);
-
-  if (isLoading) {
-    return <></>;
+  if (router.query.accessToken && router.query.refreshToken) {
+    localStorage.setItem("accessToken", `${router.query.accessToken}`);
+    localStorage.setItem("refreshToken", `${router.query.refreshToken}`);
+    queryClient.invalidateQueries({ queryKey: ["giverPost"] });
+    queryClient.invalidateQueries({ queryKey: ["takerPost"] });
   }
 
   return (
