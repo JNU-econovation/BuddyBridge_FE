87b3bf22a45d7f26abf08bc0f83201685090bcd6
minbo <smb5174@naver.com>
2024-11-28 15:43:03 +0900

    fix: 로그인 시 users/info 요청이 3번 보내지던 문제 해결

--- src/components/common/Header/User/User.tsx
+++ src/components/common/Header/User/User.tsx
@@ -1,30 +1,22 @@
-import { useEffect } from "react";
-
 import { useQuery } from "@tanstack/react-query";
 
 import Login from "@/components/common/Header/User/Login/Login";
 import NotLogin from "@/components/common/Header/User/NotLogin/NotLogin";
-import useUserInfoStore from "@/stores/kakaoInnfo";
 
 import getLogIn from "../apis/getLogIn";
 
 export default function User() {
-  const { userInfo } = useUserInfoStore();
   let accessToken;
 
   if (typeof window !== "undefined") {
     accessToken = localStorage.getItem("accessToken");
   }
 
-  const { data, isError, refetch } = useQuery({
+  const { data, isError } = useQuery({
     queryKey: ["userLogIn"],
     queryFn: () => getLogIn(),
     enabled: !!accessToken,
   });
-
-  useEffect(() => {
-    refetch();
-  }, [userInfo, refetch]);
 
   if (!accessToken) {
     <NotLogin />;
