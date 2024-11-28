43cbdd24e27d08517fbdbae6dc9a08c50a17e745
minbo <smb5174@naver.com>
2024-11-28 19:55:57 +0900

    refactor: 카카오 로그인 방식 변경

--- src/components/page-layout/HomeLayout/apis/getKakaoInfo.ts
+++
@@ -1,6 +0,0 @@
-import { axiosCertificationInstance } from "@/apis/axiosInstance";
-
-export default async function getKakaoInfo(code: string) {
-  const { data } = await axiosCertificationInstance.post("oauth/login", { authorizationCode: code });
-  return data.data;
-}
