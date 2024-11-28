4f143b48183269919e00bd0ee4562c65a7af57de
minbo <smb5174@naver.com>
2024-11-28 15:35:50 +0900

    refactor: 토큰이 필요할 수도 있는 요청에 토큰 추가

--- src/components/page-layout/HomeLayout/apis/getGiverPost.ts
+++ src/components/page-layout/HomeLayout/apis/getGiverPost.ts
@@ -1,15 +1,24 @@
+import { AxiosRequestConfig } from "axios";
+
 import { axiosInstance } from "@/apis/axiosInstance";
 
 export default async function getGiverPost() {
   const accessToken = localStorage.getItem("accessToken");
 
+  const config: AxiosRequestConfig = {
+    headers: {},
+  };
+
+  if (accessToken) {
+    config.headers = {
+      ...config.headers,
+      Authorization: `Bearer ${accessToken}`,
+    };
+  }
+
   const { data } = await axiosInstance.get(
     "posts?post-type=GIVER&page=1&size=4&sorted=modifiedAt,DESC&post-status=RECRUITING",
-    {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    },
+    config,
   );
   return data.data.content;
 }
--- src/components/page-layout/HomeLayout/apis/getTakerPost.ts
+++ src/components/page-layout/HomeLayout/apis/getTakerPost.ts
@@ -1,15 +1,24 @@
+import { AxiosRequestConfig } from "axios";
+
 import { axiosInstance } from "@/apis/axiosInstance";
 
 export default async function getTakerPost() {
   const accessToken = localStorage.getItem("accessToken");
 
+  const config: AxiosRequestConfig = {
+    headers: {},
+  };
+
+  if (accessToken) {
+    config.headers = {
+      ...config.headers,
+      Authorization: `Bearer ${accessToken}`,
+    };
+  }
+
   const { data } = await axiosInstance.get(
     "posts?post-type=TAKER&page=1&size=4&sorted=modifiedAt,DESC&post-status=RECRUITING",
-    {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    },
+    config,
   );
   return data.data.content;
 }
