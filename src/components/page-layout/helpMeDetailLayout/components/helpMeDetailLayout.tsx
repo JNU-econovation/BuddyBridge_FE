f82c024f5d309dae43471f5899de12f42157f71a
suin <suin020226@naver.com>
2024-12-07 03:06:26 +0900

    도와줄래요 게시글 상세페이지 ui 변경

--- src/components/page-layout/helpMeDetailLayout/components/helpMeDetailLayout.tsx
+++ src/components/page-layout/helpMeDetailLayout/components/helpMeDetailLayout.tsx
@@ -24,6 +24,8 @@ import Siren from "../../../../../public/icons/siren.svg"
 import getAllComment from "../../helpYouDetailLayout/apis/getAllComment";
 import deletePost from "../apis/deletePost";
 import getTakerDetail from "../apis/getTakerDetail";
+import Person from "../../../../../public/icons/personnel.svg"
+import MyInfoCard from "@/components/common/MyInfoCard/MyInfoCard";
 
 const cn = classNames.bind(styles);
 
@@ -90,26 +92,36 @@ export default function HelpMeDetailLayout() {
   }
 
   const {
-    assistanceType,
+    nickname,
+    memberId,
+    disabilityType,
+    gender,
+    profileImageUrl,
+    age
+  } = data.author;
+
+  const {
     district,
     id,
-    scheduleType,
     title,
-    scheduleDetails,
     content,
-    modifiedAt,
-    author,
-    startDate,
-    endDate,
+    createdAt,
+    assistance,
+    schedule
+  } = data.post;
+
+  const {
+    assistanceType,
     assistanceStartTime,
     assistanceEndTime,
-    gender,
-    age,
-    disabilityType,
-    headcount,
-  } = data;
+  } = assistance;
 
-  const { nickname, profileImageUrl, memberId } = author;
+  const {
+    scheduleType,
+    startDate,
+    endDate,
+    scheduleDetails,
+  } = schedule;
 
   const commentMemIds: Array<number> =
     commentData?.pages.flatMap((page) => page.content.map((comment: CommentProps) => comment.author.memberId)) || [];
@@ -122,84 +134,77 @@ export default function HelpMeDetailLayout() {
           따듯한 온정을 전하는 세상을 만듭니다.
         </header>
         <div className={cn("totalContainer")}>
-          <div className={cn("contentContainer", { isLogin: !userData })}>
-            <div className={cn("btnMenu")}>
-              {
-                userData?.memberId === data.author.memberId ? <Kebab/> : <Siren/>
-              }
-            </div>
-            <p className={cn("title")}>{title}</p>
-            <div className={cn("contentBox")}>
-              <div className={cn("profileBox")}>
-                <Image src={profileImageUrl} alt="프로필" width={35} height={35} className={cn("profileImg")} />
-                <div className={cn("textInfoBox")}>
-                  <p className={cn("nickname")}>{nickname}</p>
-                  <p className={cn("gender")}> 
-                    <span>성별</span> 
-                    <span>{gender}</span> 
-                  </p>
-                  <p className={cn("age")}> 
-                    <span>나이</span> 
-                    <span>만{age}세</span> 
-                  </p>
-                  <p className={cn("disabilityType")}> 
-                    <span>장애유형</span> 
-                    <span>{disabilityType}</span> 
-                  </p>
-                  <p className={cn("assistanceType")}> 
-                    <span>도움유형</span> 
-                    <span>{assistanceType}</span> 
-                  </p>
+          <div className={cn("btnMenu")}>
+            <Heart width={35} height={35} className={cn("likeBtn")}/>
+            {
+              userData?.memberId === data.author.memberId ? 
+                <Kebab width={30} height={30} className={cn("kebabBtn")}/> : 
+                <div className={cn("sirenBtn")}>
+                  <Siren width={25} height={25} />
+                  <span className={cn("sirenText")}>신고하기</span>
                 </div>
-              </div>
-              <div className={cn("postInfoBox")}>
-                <div className={cn("postDetailInfo")}>
-                  <p className={cn("district")}>
-                    <Location className={cn("districtIcon")}/>
-                    <span>장소</span>
-                    <span>{district}</span>
-                  </p>
-                  <p className={cn("period")}>
-                    <Calendar className={cn("calendarIcon")}/>
-                    <span>기간 &#38; 주기</span>
-                    <span className={cn("periodContent")}>
-                      <span>{scheduleType}</span>
-                      <span>{formatDateString(startDate)}</span>
-                      <span>~</span> 
-                      <span>{formatDateString(endDate)}</span>
-                      <span>{scheduleDetails}</span>
-                    </span>
-                  </p>
-                  <p className={cn("time")}>
-                    <Clock className={cn("clockIcon")}/>
-                    <span>시간</span>
-                    <span>{assistanceStartTime}</span>
-                    <span>~</span> 
-                    <span>{assistanceEndTime}</span>
-                  </p>
-                </div>
-                {/* <div className={cn("assistanceTypeContainer")}>
-                  <p className={cn("assistanceType")}>도움유형</p>
-                  <p className={cn("assistanceTypeContent")}>{assistanceType}</p>
-                </div> */}
-              </div>
-              <div className={cn("contentDetail")}>
-                <p className={cn("contentDetailLable")}>상세 내용</p>
-                <div className={cn("contentDetailBox")}>
-                <p className={cn("contentDetailTextArea")}>{content}</p>
-                </div>
-              </div>
-            </div>
-            <p className={cn("modifiedAt")}>작성일자: {formatDateString(modifiedAt)}</p>
-            {userData?.memberId === memberId && (
-              <div className={cn("buttonBox")}>
-                <button onClick={handleDeleteButtonClick} className={cn("button")}>
-                  삭제하기
-                </button>
-              </div>
-            )}
+            }
           </div>
-          {userData && (
+          <div className={cn("titleBox")}>
+            <p className={cn("title")}>{title}</p>
+            <span className={cn("postId")}>{id}번 글</span>
+          </div>
+          <div className={cn("contentBox")}>
+            <div className={cn("infoCard")}>
+              <div className={cn("profileImageBox")}>
+                <Image src={profileImageUrl} alt="프로필 사진" className={cn("profileImg")} fill/>
+              </div>
+              <div className={cn("textInfoBox")}>
+                <p className={cn("authorNickname")}>{nickname}</p>
+                <div className={cn("authorDetailInfoBox")}>
+                  <p className={cn("gender")}>성별 : {gender}</p>
+                  <p className={cn("age")}>나이 : 만 {age}세</p>
+                  <p className={cn("disabilityType")}>장애유형: {disabilityType}</p>
+                </div>
+              </div>
+            </div>
+            <div className={cn("postInfoBox")}>
+              <div className={cn("postDetailInfo")}>
+                <p className={cn("district")}>
+                  <Location className={cn("districtIcon")}/>
+                  <span className={cn("label")}>장소</span>
+                  <span>{district}</span>
+                </p>
+                <p className={cn("period")}>
+                  <Calendar className={cn("calendarIcon")}/>
+                  <span className={cn("label")}>기간 &#38; 주기</span>
+                  <span className={cn("periodContent")}>
+                    <span>{formatDateString(startDate)}</span>
+                    <span>~</span> 
+                    <span>{formatDateString(endDate)},</span>
+                    <span>{scheduleType}</span>
+                    <span>({scheduleDetails})</span>
+                  </span>
+                </p>
+                <p className={cn("time")}>
+                  <Clock className={cn("clockIcon")}/>
+                  <span className={cn("label")}>시간</span>
+                  <span>{assistanceStartTime}</span>
+                  <span>~</span> 
+                  <span>{assistanceEndTime}</span>
+                </p>
+                <p className={cn("assistanceType")}> 
+                  <Person className={cn("personIcon")}/>
+                  <span className={cn("label")}>도움유형</span> 
+                  <span>{assistanceType}</span> 
+                </p>
+              </div>
+            </div>
+            <div className={cn("contentDetail")}>
+              <p className={cn("contentDetailLabel")}>상세 내용</p>
+              <div className={cn("contentDetailBox")}>
+                {content}
+              </div>
+            </div>
+            <p className={cn("createdAt")}>작성일자: {formatDateString(createdAt)}</p>
+          </div>
+        </div>
+        {userData && (
             <>
               <div className={cn("commentBox")}>
                 {commentData?.pages.map((page) =>
@@ -222,7 +227,6 @@ export default function HelpMeDetailLayout() {
           {userData && (
             <CommentWrite id={pageId as string} user={userData as KaKaoUserInfo} commentMemIds={commentMemIds} />
           )}
-        </div>
       </div>
     </div>
   );
