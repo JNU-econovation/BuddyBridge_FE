0b55aae25b26574a33fb6411866d8925049476c1
minbo <smb5174@naver.com>
2024-12-08 10:43:51 +0900

    refactor: 찜한 목록 게시글 api 타입명 변경

--- src/components/page-layout/myPageLikesLayout/apis/getMyLikes.ts
+++ src/components/page-layout/myPageLikesLayout/apis/getMyLikes.ts
@@ -1,8 +1,8 @@
 import { axiosInstance } from "@/apis/axiosInstance";
-import { MyLikesResponse } from "@/types/post";
+import { MyLikesPostResponse } from "@/types/post";
 
 export default async function getMyLikes(pageId: string, postType: string) {
-  const { data } = await axiosInstance.get<MyLikesResponse>(
+  const { data } = await axiosInstance.get<MyLikesPostResponse>(
     `posts/likes/my-page?post-type=${postType}&page=${pageId}&size=4`,
   );
   return data.data;
