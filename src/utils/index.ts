0c057c2b838fdc8ca40da7eeb55dd3adc2eb20f1
minbo <smb5174@naver.com>
2024-12-08 11:26:43 +0900

    refactor: 매개 변수 타입 변경

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -8,7 +8,7 @@ export function formatDateString(dateString: Date) {
   return `${year}.${month}.${day}`;
 }
 
-export function formatAllDateExpectSecondsString(dateString: string) {
+export function formatAllDateExpectSecondsString(dateString: Date) {
   const date = new Date(dateString);
 
   const year = date.getFullYear();
