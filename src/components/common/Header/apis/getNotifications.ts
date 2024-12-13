f63d3a03d018b594d8298f05ba22c05a93d8feb7
suin <suin020226@naver.com>
2024-12-13 12:44:23 +0900

    타입과 읽음 여부 쿼리에 포함

--- src/components/common/Header/apis/getNotifications.ts
+++ src/components/common/Header/apis/getNotifications.ts
@@ -2,10 +2,10 @@ import { axiosInstance } from "@/apis/axiosInstance";
 
 export default async function getNotifications(limit: number, cursor: number, type: string, isRead: string) {
   if (cursor !== 0) {
-    const { data } = await axiosInstance.get(`notifications?limit=${limit}&cursor=${cursor}`);
+    const { data } = await axiosInstance.get(`notifications?limit=${limit}&cursor=${cursor}&type=${type}&is-read=${isRead}`);
     return data.data;
   } else {
-    const { data } = await axiosInstance.get(`notifications?limit=${limit}`);
+    const { data } = await axiosInstance.get(`notifications?limit=${limit}&type=${type}&is-read=${isRead}`);
     return data.data;
   }
 }
