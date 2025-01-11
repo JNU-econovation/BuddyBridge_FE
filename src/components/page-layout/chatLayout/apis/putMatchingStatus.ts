fcb9c27cb0b83218107fc62d2f443c0aedd692e0
minbo <smb5174@naver.com>
2025-01-11 21:18:42 +0900

    refactor: 채팅방 상태 변경 api 수정

--- src/components/page-layout/chatLayout/apis/putMatchingStatus.ts
+++ src/components/page-layout/chatLayout/apis/putMatchingStatus.ts
@@ -1,8 +1,11 @@
 import { axiosInstance } from "@/apis/axiosInstance";
 
 export default async function putMatchingStatus(chatingRoomId: number, status: string) {
-  const { data } = await axiosInstance.put(`matching/${chatingRoomId}`, {
-    matchingStatus: status,
-  });
+  const { data } = await axiosInstance.put(
+    `matching/${chatingRoomId}`,
+    JSON.stringify({
+      matchingStatusEvent: status,
+    }),
+  );
   return data.data;
 }
