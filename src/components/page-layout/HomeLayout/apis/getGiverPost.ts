407293fbb5085231121cddb93b30685be999be6d
minbo <smb5174@naver.com>
2025-01-15 22:15:06 +0900

    refactor: 메인 페이지 api 요청 수정

--- src/components/page-layout/HomeLayout/apis/getGiverPost.ts
+++ src/components/page-layout/HomeLayout/apis/getGiverPost.ts
@@ -3,7 +3,7 @@ import { PostListResponse } from "@/types/post";
 
 export default async function getGiverPost() {
   const { data } = await axiosInstance.get<PostListResponse>(
-    "posts?post-type=GIVER&page=1&size=4&sorted=modifiedAt,DESC&post-status=RECRUITING",
+    "posts?post-type=GIVER&page=0&size=4&sorted=modifiedAt,DESC&post-status=RECRUITING",
   );
   return data.data.content;
 }
