63dd6ef1241d9dc49a06b54945794ab0ee63be58
minbo <smb5174@naver.com>
2025-01-17 18:57:43 +0900

    refactor: 소셜 로그인 구현 동작 방식 변경

--- src/components/page-layout/HomeLayout/components/HomeLayout.tsx
+++ src/components/page-layout/HomeLayout/components/HomeLayout.tsx
@@ -1,22 +1,7 @@
-import { useQueryClient } from "@tanstack/react-query";
-
-import { useRouter } from "next/router";
-
 import Banner from "@/components/page-layout/HomeLayout/components/Banner/Banner";
 import PostList from "@/components/page-layout/HomeLayout/components/PostList/PostList";
 
 export default function HomeLayout() {
-  const router = useRouter();
-  const queryClient = useQueryClient();
-
-  if (router.query.accessToken && router.query.refreshToken) {
-    localStorage.setItem("accessToken", `${router.query.accessToken}`);
-    localStorage.setItem("refreshToken", `${router.query.refreshToken}`);
-    queryClient.invalidateQueries({ queryKey: ["userLogIn"] });
-    queryClient.invalidateQueries({ queryKey: ["giverPost"] });
-    queryClient.invalidateQueries({ queryKey: ["takerPost"] });
-  }
-
   return (
     <>
       <Banner />
