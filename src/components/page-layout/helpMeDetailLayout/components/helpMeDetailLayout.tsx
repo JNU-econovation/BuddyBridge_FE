22b90fdd5b9127ce93fe5509eb8b41283b00c091
suin <suin020226@naver.com>
2025-01-18 16:47:47 +0900

    refactor: 댓글 상세페이지, 마이페이지 매칭 봉사 페이지 스타일 수정

--- src/components/page-layout/helpMeDetailLayout/components/helpMeDetailLayout.tsx
+++ src/components/page-layout/helpMeDetailLayout/components/helpMeDetailLayout.tsx
@@ -240,8 +240,8 @@ function Main( data:any) {
               <p className={cn("contentDetailLabel")}>상세 내용</p>
               <div className={cn("contentDetailBox")}>{content}</div>
             </div>
-            <p className={cn("createdAt")}>작성일자: {formatDateString(createdAt)}</p>
           </div>
+          <p className={cn("createdAt")}>작성일자: {formatDateString(createdAt)}</p>
         </div>
         {userData && (
           <>
