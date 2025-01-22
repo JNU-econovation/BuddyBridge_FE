5c1f573d8d32b0e92d504f4dcc0676e8ea493f57
suin <suin020226@naver.com>
2025-01-23 03:38:04 +0900

    봉사 완료 확인시 확인모달 안뜨는 문제 해결

--- src/components/page-layout/myPageFinishedLayout/components/MyFinishedListBox/MyFinishedListBox.tsx
+++ src/components/page-layout/myPageFinishedLayout/components/MyFinishedListBox/MyFinishedListBox.tsx
@@ -56,7 +56,12 @@ export default function MyFinishedListBox() {
       <AssistanceTypeFilter pageId={`${pageId}`} memberRole={memberRole} />
       <div className={cn("myFinishedListBox")}>
         <CompleteToggle isOn={isToggleOn} toggleSwitch={toggleSwitch} />
-        <MyFinishedList finishedList={FinishedList.content} memberRole={memberRole} />
+        <MyFinishedList
+          finishedList={FinishedList.content}
+          memberRole={memberRole}
+          pageId={pageId}
+          isToggleOn={isToggleOn}
+        />
         <div className={cn("paginationBox")}>
           <Pagination
             currentPage={Number(pageId)}
