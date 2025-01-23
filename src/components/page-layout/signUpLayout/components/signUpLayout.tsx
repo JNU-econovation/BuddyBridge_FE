1f82d3e3b17f82c0d048731f73ea1437942b113b
minbo <smb5174@naver.com>
2025-01-23 21:28:29 +0900

    refactor: 이름에 특수 문자를 입력 못하게 변경

--- src/components/page-layout/signUpLayout/components/signUpLayout.tsx
+++ src/components/page-layout/signUpLayout/components/signUpLayout.tsx
@@ -29,7 +29,11 @@ import postSignUp from "../apis/postSignUp";
 const cn = classNames.bind(styles);
 
 const signUpSchema = z.object({
-  name: z.string().min(2, "이름은 최소 2자 이상이어야 합니다.").max(18, "이름은 최대 18자입니다."),
+  name: z
+    .string()
+    .min(2, "이름은 최소 2자 이상이어야 합니다.")
+    .max(18, "이름은 최대 18자입니다.")
+    .regex(/^[^\s!@#$%^&*()_+={}\[\]:;"'<>,.?~`\\/-]+$/, "이름에는 특수문자를 포함할 수 없습니다."),
   nickname: z
     .string()
     .min(2, "닉네임은 최소 2자 이상이어야 합니다.")
