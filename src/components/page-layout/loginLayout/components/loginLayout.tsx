609a530f459bc432b9b38da416df5e9e28f9269d
minbo <smb5174@naver.com>
2025-01-23 10:28:49 +0900

    refactor: 카카오 로그인만 제공한다는 문구 삭제

--- src/components/page-layout/loginLayout/components/loginLayout.tsx
+++ src/components/page-layout/loginLayout/components/loginLayout.tsx
@@ -139,7 +139,6 @@ export default function LoginLayout() {
           </button>
         </div>
       </div>
-      <p className={cn("loginInfo")}>※ 사용자의 신원을 보장하기 위해 카카오 로그인만 제공합니다. </p>
     </article>
   );
 }
